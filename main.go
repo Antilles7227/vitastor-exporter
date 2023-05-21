7eb6675f7c0d6cc370e82ebddb514864c4552e25
Dmitry Yusko <d.yusko@vk.team>
2023-05-21 18:39:02 +0500

    Finish basic pool scraping

--- main.go
+++ main.go
@@ -6,30 +6,17 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"time"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
-	clientv3 "go.etcd.io/etcd/client/v3"
 	vconfig "github.com/Antilles7227/vitastor-exporter/config"
-)
-
-
-
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-			Name: "myapp_processed_ops_total",
-			Help: "The total number of processed events",
-	})
+	exporter "github.com/Antilles7227/vitastor-exporter/exporter"
 )
 
 
 func main() {
 	portArg := flag.Int("port", 8080, "Port to expose metrics. Default: 8080")
 	uriArg := flag.String("metrics-path", "/metrics", "Path to expose metrics. Default: /metrics")
-	refreshIntArg := flag.Int("refresh-interval", 5, "Etcd check interval in seconds. Default: 5")
 	vitastorConfArg := flag.String("vitastor-conf", "/etc/vitastor/vitastor.conf", "Path to vitastor.conf (to obtain etcd connection params). Default: /etc/vitastor/vitastor.conf")
 	etcdUrlArg := flag.String("etcd-url", "", "Comma-separated list of etcd urls. WARNING: setting that param will override --vitastor-conf. Default: empty")
 	vitastorPrefix := flag.String("vitastor-prefix", "/vitastor", "Etcd tree prefix for Vitastor cluster info. Default: /vitastor")
@@ -49,20 +36,11 @@ func main() {
 	}
 
 	if *etcdUrlArg != "" {
-		log.Info("etcdUrlArg is set, overriding param in ")
+		log.Info("etcdUrlArg is set, overriding param in vitastor.conf")
+		config.VitastorEtcdUrls = strings.Split(*etcdUrlArg, ",")
 	}
-
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   config.VitastorEtcdUrls,
-		DialTimeout: 5 * time.Second,
-	})
-	if err != nil {
-		log.Error(err, "Unable to connect to etcd")
-		return
-	}
-	defer cli.Close()
-
-	go getMetrics(config, *refreshIntArg)
+	
+	exporter.Register(&config)
 
 	http.Handle(*uriArg, promhttp.Handler())
     http.ListenAndServe(":" + string(int32(*portArg)), nil)
